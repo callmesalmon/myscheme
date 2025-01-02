a706e8409bd729324cdecab9259f7822600db84d
eliss <elis.staaf@proton.me>
2025-01-02 22:01:47 +0100

    [feat]: Add a ``rember()`` code example

---
+++ scheme/rember.scm
@@ -0,0 +1,11 @@
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) a) (cdr lat))
+      (else (cons (car lat)
+                  (rember a (cdr lat)))))))
+
+(rember 'mint '(lamb chops and mint flavored mint jelly)) ; '(lamb chops and flavored mint jelly)
+(rember 'toast '(bacon lettuce and tomato))               ; '(bacon lettuce and tomato)
+(rember 'cup '(coffee cup tea cup and hick cup))          ; '(coffee tea cup and hick cup)
