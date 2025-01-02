32fff57d4d2dc4038be6078546b11091082e4589
eliss <elis.staaf@proton.me>
2025-01-02 22:07:22 +0100

    [feat] Add atom code examples in atom.scm

---
+++ scheme/atom.scm
@@ -0,0 +1,19 @@
+(define (zero? n)
+    (if (= n 0) #t #f))
+
+(define atom?
+ (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (o+ (car tup) (addtup (cdr tup)))))))
