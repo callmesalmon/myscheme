7164d4c1e3de849c188e42fb623489c7d36e6532
eliss <elis.staaf@proton.me>
2025-01-03 00:48:51 +0100

    Add a helper script

---
+++ scheme/help.scm
@@ -0,0 +1,12 @@
+; Scheme library for help
+; with MyScheme. Simple.
+
+(define version "1.0.0")
+
+"MYSCHEME"
+"    MyScheme is a Scheme interpreter I hacked"
+"    together in an afternoon. It's... Functional."
+"USAGE"
+"    myscm <args>"
+"FLAGS"
+"    -d: Enable debug mode."
