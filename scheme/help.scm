0a9134a38e37845f531898dc3014c82afc4360fa
eliss <elis.staaf@proton.me>
2025-01-03 12:18:41 +0100

    [feat] Add a "planned features" section to ``scheme/help.scm``

--- scheme/help.scm
+++ scheme/help.scm
@@ -9,3 +9,7 @@
 (write "FLAGS"                                            )
 (write "    -d: Enable debug mode."                       )
 (write "    -v: Print version and exit."                  )
+(write "TODO"                                             )
+(write "    - Add macro support."                         )
+(write "    - Add user input support."                    )
+(write "    - Add GUI support."                           )
