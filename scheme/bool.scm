8b77accbbf4f8c4ae3227982ab66930f727974d8
eliss <elis.staaf@proton.me>
2025-01-03 11:37:40 +0100

    [fix] Update bool example to... Print something.
    Y'know, since return values were discontinued, you can no longer:
    
        "Oi mate!"
    
    You need to ``write`` Oi mate:
    
        (write "Oi mate!")
    
    So yeah, add bool_to_str and everyone's happy

--- scheme/bool.scm
+++ scheme/bool.scm
@@ -1,5 +1,12 @@
 (define computer_on_fire #f)
 (define scheme_is_running #t)
 
-(boolean? computer_on_fire)  ; #t ($computer_on_fire == bool)
-(boolean? scheme_is_running) ; #t ($scheme_is_running == bool)
+(define (bool_to_str v)
+    (if (boolean? v)
+        (if (= v #t)
+            "True"
+            "False")
+        "Null"))
+
+(write (bool_to_str (computer_on_fire)))  ; "True" ($computer_on_fire == bool)
+(write (bool_to_Str (scheme_is_running))) ; "True" ($scheme_is_running == bool)
