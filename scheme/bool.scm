006cd26d233a00561d02473d110cae486df7dd3b
eliss <elis.staaf@proton.me>
2025-01-06 13:21:55 +0100

    [fix] ``bool.scm``

--- scheme/bool.scm
+++ scheme/bool.scm
@@ -1,5 +1,5 @@
 (define computer_on_fire #f)
 (define scheme_is_running #t)
 
-(write (bool_to_str computer_on_fire))  ; "True" ($computer_on_fire == bool)
-(write (bool_to_str scheme_is_running)) ; "True" ($scheme_is_running == bool)
+(write (bool_to_str computer_on_fire))  ; "False" ($computer_on_fire)
+(write (bool_to_str scheme_is_running)) ; "True" ($scheme_is_running)
