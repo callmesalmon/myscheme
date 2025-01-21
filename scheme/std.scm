b663aabd25ae731a8481cb4c13a25ecc8c7cf48e
eliss <elis.staaf@proton.me>
2025-01-21 22:32:00 +0100

    [fix] stdlib formatting
    That happened I guess

--- scheme/std.scm
+++ scheme/std.scm
@@ -6,7 +6,7 @@
 ; If you want the best experience with
 ; MyScheme, please Copy-Paste the
 ; following into your project:
-;     
+;
 ;     (load "scheme/std.scm")
 
 (define zero 0)
@@ -23,7 +23,7 @@
 ; (even 5) ; #f
 ; (even 6) ; #t
 (define (even? n)
-    (if (integer? n)  
+    (if (integer? n)
         (if (= (mod n 2) 0)
             #t
             #f)
@@ -100,36 +100,36 @@
       1         ; 1! = 1
       n * (facto n - 1)))
 
-(define (caar x) (car (car x))) 
-(define (cadr x) (car (cdr x))) 
-(define (cddr x) (cdr (cdr x))) 
-(define (cdar x) (cdr (car x))) 
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cddr x) (cdr (cdr x)))
+(define (cdar x) (cdr (car x)))
 
-(define (caaar x) (car (car (car x)))) 
-(define (caadr x) (car (car (cdr x)))) 
-(define (cadar x) (car (cdr (car x)))) 
-(define (caddr x) (car (cdr (cdr x)))) 
-(define (cdaar x) (cdr (car (car x)))) 
-(define (cdadr x) (cdr (car (cdr x)))) 
-(define (cddar x) (cdr (cdr (car x)))) 
-(define (cdddr x) (cdr (cdr (cdr x)))) 
+(define (caaar x) (car (car (car x))))
+(define (caadr x) (car (car (cdr x))))
+(define (cadar x) (car (cdr (car x))))
+(define (caddr x) (car (cdr (cdr x))))
+(define (cdaar x) (cdr (car (car x))))
+(define (cdadr x) (cdr (car (cdr x))))
+(define (cddar x) (cdr (cdr (car x))))
+(define (cdddr x) (cdr (cdr (cdr x))))
 
-(define (caaaar x) (car (car (car (car x))))) 
-(define (caaadr x) (car (car (car (cdr x))))) 
-(define (caadar x) (car (car (cdr (car x))))) 
-(define (caaddr x) (car (car (cdr (cdr x))))) 
-(define (cadaar x) (car (cdr (car (car x))))) 
-(define (cadadr x) (car (cdr (car (cdr x))))) 
-(define (caddar x) (car (cdr (cdr (car x))))) 
-(define (cadddr x) (car (cdr (cdr (cdr x))))) 
-(define (cdaaar x) (cdr (car (car (car x))))) 
-(define (cdaadr x) (cdr (car (car (cdr x))))) 
-(define (cdadar x) (cdr (car (cdr (car x))))) 
-(define (cdaddr x) (cdr (car (cdr (cdr x))))) 
-(define (cddaar x) (cdr (cdr (car (car x))))) 
-(define (cddadr x) (cdr (cdr (car (cdr x))))) 
-(define (cdddar x) (cdr (cdr (cdr (car x))))) 
-(define (cddddr x) (cdr (cdr (cdr (cdr x))))) 
+(define (caaaar x) (car (car (car (car x)))))
+(define (caaadr x) (car (car (car (cdr x)))))
+(define (caadar x) (car (car (cdr (car x)))))
+(define (caaddr x) (car (car (cdr (cdr x)))))
+(define (cadaar x) (car (cdr (car (car x)))))
+(define (cadadr x) (car (cdr (car (cdr x)))))
+(define (caddar x) (car (cdr (cdr (car x)))))
+(define (cadddr x) (car (cdr (cdr (cdr x)))))
+(define (cdaaar x) (cdr (car (car (car x)))))
+(define (cdaadr x) (cdr (car (car (cdr x)))))
+(define (cdadar x) (cdr (car (cdr (car x)))))
+(define (cdaddr x) (cdr (car (cdr (cdr x)))))
+(define (cddaar x) (cdr (cdr (car (car x)))))
+(define (cddadr x) (cdr (cdr (car (cdr x)))))
+(define (cdddar x) (cdr (cdr (cdr (car x)))))
+(define (cddddr x) (cdr (cdr (cdr (cdr x)))))
 
 ; null
 (define null '())
@@ -165,9 +165,9 @@
                    (- counter 1)
                    (* b product))))
   (expt-iter b n 1))
- 
+
 (define (gcd a b) (if (= b 0) a (gcd b (remainder a b))))
- 
+
 ; list
 (define (assoc x y)
   (cond ((null? y) '())
@@ -191,7 +191,7 @@
     (if (null? slist)
       '()
        (cons
-	     (subst-in-s-exp new old (car slist)) 
+	     (subst-in-s-exp new old (car slist))
 	     (subst new old (cdr slist))))))
 
 (define subst-in-s-exp
