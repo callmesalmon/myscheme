6c63f6a44ffcf65d2833a6da5942e0fd4d4d815d
salmon <staaf.elis@gmail.com>
2026-04-27 01:00:31 +0200

    follow style guidelines in ``config.sh``

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 
-STDLIB_DIR=/usr/local/share/scheme/
-STDLIB_FILE=myscheme-stdlib.scm
+STDLIB_DIR="/usr/local/share/scheme/"
+STDLIB_FILE="myscheme-stdlib.scm"
 
 if [ ! -d "$STDLIB_DIR" ]; then
     mkdir "$STDLIB_DIR"
 fi
 
-cp scheme/stdlib.scm $STDLIB_DIR/$STDLIB_FILE
+cp "scheme/stdlib.scm" "$STDLIB_DIR/$STDLIB_FILE"
