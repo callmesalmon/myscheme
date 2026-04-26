8a0680acbc15204764e8f2c127c06ca18380f302
salmon <staaf.elis@gmail.com>
2026-04-27 01:04:24 +0200

    remove useless arithmetic function aliases

--- scheme/stdlib.scm
+++ scheme/stdlib.scm
@@ -139,9 +139,6 @@
   (- 0 x))
 
 (define (square x) (* x x))
-(define (cube x)(* x x x))
-(define (inc x) (+ x 1))
-(define (dec x) (- x 1))
 (define (abs x) (if (> x 0) x (invert x)))
 
 ; Exponents!
